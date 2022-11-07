b2704d26db83a5a2c575ac922e4d286deaeed274
michalak_pawel@wp.pl <pawel.michalak@primesoft.pl>
2022-11-07 18:29:23 +0100

    Rozwiązanie zadania2

---
+++ Bank/KIR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Bank
+{
+    public static class KIR
+    {
+        public static Dictionary<string, Bank> tabelaBanków { get; private set; }
+
+        public static void przekazPrzelew(Przelew przelew)
+        {
+            var oddzialBanku = przelew.BankOdbiorcy;
+
+            oddzialBanku.Przelew(przelew.RachNadawcy, przelew.BankNadawcy, przelew.RachOdbiorcy, przelew.BankOdbiorcy, (int)przelew.KwotaPrzelewu);
+        }
+
+        public static Bank szukajBank(string nazwa)
+        {
+            if (tabelaBanków[nazwa] != null)
+            {
+                return tabelaBanków[nazwa];
+            }
+            else return null;
+        }
+
+    }
+}
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Bank
+﻿using System;
+
+namespace Bank
 {
     class Program
     {
         static void Main(string[] args)
         {
             Bank pkoBp = new Bank();
+
+            KIR.tabelaBanków.Add("pkoBp", pkoBp);
+            
             Rachunek r11 = pkoBp.ZalozRachunek("11", "Jan", "Kowalski");
             r11.Wplata(200);
             
@@ -14,12 +19,12 @@
             Rachunek r15 = pkoBp.ZalozRachunek("15", "Kazimierz", "Nijaki");
             r11.Wplata(500);
             
-            pkoBp.Przelew("13", "11", 100);
-            pkoBp.Przelew(r15, r11, 100);
+            pkoBp.Przelew("13", "pkoBP", "11", "pkoBP", 100);
+            pkoBp.Przelew(r15, pkoBp, r11, pkoBp, 100);
             
-            Console.Writeline("R11: " + r11.Saldo());
-            Console.Writeline("R13: " + r13.Saldo());
-            Console.Writeline("R15: " + r15.Saldo());
+            Console.WriteLine("R11: " + r11.Saldo());
+            Console.WriteLine("R13: " + r13.Saldo());
+            Console.WriteLine("R15: " + r15.Saldo());
         }
     }
 }
